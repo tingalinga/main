490b420861958421498faed02b36dbbef235a71a
joel-lim <joellimaojun@hotmail.com>
2020-02-21 13:29:32 +0800

    Change Transaction to remove equals method and only use one Tag

--- src/main/java/seedu/address/model/wallet/Income.java
+++ src/main/java/seedu/address/model/wallet/Income.java
@@ -1,8 +1,6 @@
 package seedu.address.model.wallet;
 
 import java.time.LocalDate;
-import java.util.Objects;
-import java.util.Set;
 
 import seedu.address.model.tag.Tag;
 
@@ -14,34 +12,8 @@ public class Income extends Transaction {
     /**
      * Every field must be present and not null.
      */
-    public Income(Description description, Amount amount, LocalDate date, Set<Tag> tags) {
-        super(description, amount, date, tags);
-    }
-
-    /**
-     * Returns true if both Incomes have the same data fields.
-     */
-    @Override
-    public boolean equals(Object other) {
-        if (other == this) {
-            return true;
-        }
-
-        if (!(other instanceof Income)) {
-            return false;
-        }
-
-        Income otherIncome = (Income) other;
-        return otherIncome.getDescription().equals(getDescription())
-                && otherIncome.getAmount().equals(getAmount())
-                && otherIncome.getDate().equals(getDate())
-                && otherIncome.getTags().equals(getTags());
-    }
-
-    @Override
-    public int hashCode() {
-        // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash("income", description, amount, date, tags);
+    public Income(Description description, Amount amount, LocalDate date, Tag tag) {
+        super(description, amount, date, tag);
     }
 
 }
