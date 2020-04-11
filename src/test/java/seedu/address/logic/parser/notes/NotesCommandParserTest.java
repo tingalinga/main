6ca6f93cdaa39a4e829efa722ba8088181f49fc5
jerrenss <>
2020-04-11 18:05:10 +0800

    Tested more Parsers

--- src/test/java/seedu/address/logic/parser/notes/NotesCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/notes/NotesCommandParserTest.java
@@ -1,11 +1,37 @@
 package seedu.address.logic.parser.notes;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.logic.commands.notes.NotesCommand;
+import seedu.address.logic.commands.notes.NotesExportCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
 public class NotesCommandParserTest {
+
+    public static final String INVALID_COMMAND_SUPPLIED = "addition";
+    public static final String VALID_COMMAND_SUPPLIED = "export";
+
+    @Test
+    public void invalidRequestSupplied_throwsParseException() {
+        NotesCommandParser parser = new NotesCommandParser();
+        String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, "Notes does not have function "
+                + "requested. Enter 'notes' for help.");
+        assertParseFailure(parser, INVALID_COMMAND_SUPPLIED, expectedMessage);
+    }
+
+    @Test
+    public void parseSuccess() throws ParseException {
+        NotesCommandParser parser = new NotesCommandParser();
+        NotesCommand expected = new NotesExportCommand();
+        assertEquals(parser.parse(VALID_COMMAND_SUPPLIED), expected);
+    }
+
     @Test
     public void equals() {
         NotesCommandParser firstParser = new NotesCommandParser();
--- src/test/java/seedu/address/logic/parser/notes/NotesDeleteCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/notes/NotesDeleteCommandParserTest.java
@@ -1,11 +1,23 @@
 package seedu.address.logic.parser.notes;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.notes.NotesDeleteCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
 public class NotesDeleteCommandParserTest {
+
+    @Test
+    public void parse_success() throws ParseException {
+        NotesDeleteCommandParser parser = new NotesDeleteCommandParser();
+        Index index = Index.fromOneBased(4);
+        assertEquals(new NotesDeleteCommand(index), parser.parse(" 4"));
+    }
 
     @Test
     public void equals() {
