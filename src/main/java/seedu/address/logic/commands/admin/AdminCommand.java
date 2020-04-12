002ac8971633371eb5a9f13cad3dc87c15104dbb
tingalinga <itshuitingg@gmail.com>
2020-04-13 06:30:09 +0800

    more UGDG edits

--- src/main/java/seedu/address/logic/commands/admin/AdminCommand.java
+++ src/main/java/seedu/address/logic/commands/admin/AdminCommand.java
@@ -16,13 +16,14 @@ public abstract class AdminCommand extends Command {
     public static final String ADMIN_DELETE = "delete";
     public static final String ADMIN_DATES = "dates";
 
-    public static final String MESSAGE_USAGE = "These are the various admin commands:\n"
-            + COMMAND_WORD + ": displays the last updated admin list of the class\n"
-            + COMMAND_WORD + " " + ADMIN_DATES + ": displays the list of dates that has admin information of the "
-            + "class\n"
-            + COMMAND_WORD + " " + ADMIN_SAVE + ": saves the last updated admin list of class as today's date\n"
-            + COMMAND_WORD + " " + ADMIN_FETCH + " YYYY-MM-DD: fetches the admin details of class at specified date\n"
-            + COMMAND_WORD + " " + ADMIN_DELETE + " YYYY-MM-DD: deletes the admin details of class at specified "
+    public static final String MESSAGE_USAGE = "The Administration feature stores all attendance and temperature:"
+            + '\n' + "[HELP ON ADMIN COMMANDS]\n"
+            + "1. " + COMMAND_WORD + ": displays the last updated admin list of the class\n"
+            + "2. " + COMMAND_WORD + " " + ADMIN_DATES + ": displays the list of dates that has admin information of "
+            + "the class\n"
+            + "3. " + COMMAND_WORD + " " + ADMIN_SAVE + ": saves the last updated admin list of class as today's date\n"
+            + "4. " + COMMAND_WORD + " " + ADMIN_FETCH + " YYYY-MM-DD: fetches the admin details of class at specified date\n"
+            + "5. " + COMMAND_WORD + " " + ADMIN_DELETE + " YYYY-MM-DD: deletes the admin details of class at specified "
             + "date\n";
 
     @Override
