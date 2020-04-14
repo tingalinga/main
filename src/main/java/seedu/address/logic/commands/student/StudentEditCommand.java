254ce2dccbb348bfd05ebbec43fba5b9e7e04f86
tingalinga <itshuitingg@gmail.com>
2020-04-15 01:56:52 +0800

    Fix StudentEditCommand bug

--- src/main/java/seedu/address/logic/commands/student/StudentEditCommand.java
+++ src/main/java/seedu/address/logic/commands/student/StudentEditCommand.java
@@ -85,7 +85,7 @@ public class StudentEditCommand extends StudentCommand {
         Student studentToEdit = lastShownList.get(index.getZeroBased());
         Student editedStudent = createEditedStudent(studentToEdit, editStudentDescriptor);
 
-        if (model.hasStudentName(editedStudent.getName().fullName)) {
+        if (editStudentDescriptor.getName().isPresent() && model.hasStudentName(editStudentDescriptor.name.fullName)) {
             throw new CommandException(MESSAGE_DUPLICATE_STUDENT);
         }
 
